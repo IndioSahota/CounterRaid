e74ebcd9836fa59bee8e97cda0b492821562d62b
Indio Sahota <isahota4@uwo.ca>
2021-09-19 03:40:53 -0400

    main changes

--- camera_REMOTE_493.py
+++ camera_REMOTE_493.py
@@ -1,10 +1,6 @@
 import cv2
 
-<<<<<<< Updated upstream
-def use_camera(self):
-=======
 def use_camera():
->>>>>>> Stashed changes
     webcam = cv2.VideoCapture(0)
     check, frame = webcam.read()
     cv2.imwrite(filename=r'images\image.jpg', img=frame)
