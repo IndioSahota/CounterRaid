9a38d3afadabeb85cdd74b5b197af59cf4d2a146
Indio Sahota <isahota4@uwo.ca>
2021-09-18 23:22:22 -0400

    reformated geolocator and camera

---
+++ geolocator.py
@@ -0,0 +1,16 @@
+import requests
+import urllib.request
+
+def get_location():
+    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    response = requests.get("http://ip-api.com/json/" + external_ip).json()
+    location = {'city': response['city'],
+                'region': response['region'],
+                'country': response['country'],
+                'latitude': response['lat'],
+                'longitude': response['lon'],
+                'isp': response['isp'],
+                'ip address': response['query']}
+    return location
+
+print(get_location())
